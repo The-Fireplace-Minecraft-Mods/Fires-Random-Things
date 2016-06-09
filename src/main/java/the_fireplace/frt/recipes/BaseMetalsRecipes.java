848a6e45657b10e3bb162dbf34bc1540ba808340
The_Fireplace <the.f1repl4ce@gmail.com>
2016-06-08 20:28:46 -0500

    Switched to use OreDictionary for Base Metals Paxel recipes

--- src/main/java/the_fireplace/frt/recipes/BaseMetalsRecipes.java
+++ src/main/java/the_fireplace/frt/recipes/BaseMetalsRecipes.java
@@ -11,21 +11,6 @@ import the_fireplace.frt.compat.basemetals.RegisterBaseMetals;
 public class BaseMetalsRecipes extends VanillaStacks implements IRecipeRegister {
     ItemStack goldDustStack = new ItemStack(Items.gold_powder);
     ItemStack ironDustStack = new ItemStack(Items.iron_powder);
-    ItemStack copperStack = new ItemStack(Items.copper_ingot);
-    ItemStack silverStack = new ItemStack(Items.silver_ingot);
-    ItemStack tinStack = new ItemStack(Items.tin_ingot);
-    ItemStack leadStack = new ItemStack(Items.lead_ingot);
-    ItemStack nickelStack = new ItemStack(Items.nickel_ingot);
-    ItemStack bronzeStack = new ItemStack(Items.bronze_ingot);
-    ItemStack brassStack = new ItemStack(Items.brass_ingot);
-    ItemStack steelStack = new ItemStack(Items.steel_ingot);
-    ItemStack invarStack = new ItemStack(Items.invar_ingot);
-    ItemStack electrumStack = new ItemStack(Items.electrum_ingot);
-    ItemStack coldironStack = new ItemStack(Items.coldiron_ingot);
-    ItemStack mithrilStack = new ItemStack(Items.mithril_ingot);
-    ItemStack adamantineStack = new ItemStack(Items.adamantine_ingot);
-    ItemStack starsteelStack = new ItemStack(Items.starsteel_ingot);
-    ItemStack aquariumStack = new ItemStack(Items.aquarium_ingot);
 
     ItemStack copperPaxelStack = new ItemStack(RegisterBaseMetals.copper_paxel);
     ItemStack silverPaxelStack = new ItemStack(RegisterBaseMetals.silver_paxel);
@@ -52,21 +37,21 @@ public class BaseMetalsRecipes extends VanillaStacks implements IRecipeRegister 
         ShattererRegistry.registerPopFurnaceRecipe(goldIngotStack, goldDustStack, 1);
         ShattererRegistry.registerPopFurnaceRecipe(ironStack, ironDustStack, 1);
 
-        shaped(adamantinePaxelStack, "iii", " i ", " s ", 'i', adamantineStack, 's', "stickWood");
-        shaped(copperPaxelStack, "iii", " i ", " s ", 'i', copperStack, 's', "stickWood");
-        shaped(silverPaxelStack, "iii", " i ", " s ", 'i', silverStack, 's', "stickWood");
-        shaped(tinPaxelStack, "iii", " i ", " s ", 'i', tinStack, 's', "stickWood");
-        shaped(leadPaxelStack, "iii", " i ", " s ", 'i', leadStack, 's', "stickWood");
-        shaped(nickelPaxelStack, "iii", " i ", " s ", 'i', nickelStack, 's', "stickWood");
-        shaped(bronzePaxelStack, "iii", " i ", " s ", 'i', bronzeStack, 's', "stickWood");
-        shaped(brassPaxelStack, "iii", " i ", " s ", 'i', brassStack, 's', "stickWood");
-        shaped(steelPaxelStack, "iii", " i ", " s ", 'i', steelStack, 's', "stickWood");
-        shaped(invarPaxelStack, "iii", " i ", " s ", 'i', invarStack, 's', "stickWood");
-        shaped(electrumPaxelStack, "iii", " i ", " s ", 'i', electrumStack, 's', "stickWood");
-        shaped(coldironPaxelStack, "iii", " i ", " s ", 'i', coldironStack, 's', "stickWood");
-        shaped(mithrilPaxelStack, "iii", " i ", " s ", 'i', mithrilStack, 's', "stickWood");
-        shaped(starsteelPaxelStack, "iii", " i ", " s ", 'i', starsteelStack, 's', "stickWood");
-        shaped(aquariumPaxelStack, "iii", " i ", " s ", 'i', aquariumStack, 's', "stickWood");
+        shaped(adamantinePaxelStack, "iii", " i ", " s ", 'i', "ingotAdamantine", 's', "stickWood");
+        shaped(copperPaxelStack, "iii", " i ", " s ", 'i', "ingotCopper", 's', "stickWood");
+        shaped(silverPaxelStack, "iii", " i ", " s ", 'i', "ingotSilver", 's', "stickWood");
+        shaped(tinPaxelStack, "iii", " i ", " s ", 'i', "ingotTin", 's', "stickWood");
+        shaped(leadPaxelStack, "iii", " i ", " s ", 'i', "ingotLead", 's', "stickWood");
+        shaped(nickelPaxelStack, "iii", " i ", " s ", 'i', "ingotNickel", 's', "stickWood");
+        shaped(bronzePaxelStack, "iii", " i ", " s ", 'i', "ingotBronze", 's', "stickWood");
+        shaped(brassPaxelStack, "iii", " i ", " s ", 'i', "ingotBrass", 's', "stickWood");
+        shaped(steelPaxelStack, "iii", " i ", " s ", 'i', "ingotSteel", 's', "stickWood");
+        shaped(invarPaxelStack, "iii", " i ", " s ", 'i', "ingotInvar", 's', "stickWood");
+        shaped(electrumPaxelStack, "iii", " i ", " s ", 'i', "ingotElectrum", 's', "stickWood");
+        shaped(coldironPaxelStack, "iii", " i ", " s ", 'i', "ingotColdiron", 's', "stickWood");
+        shaped(mithrilPaxelStack, "iii", " i ", " s ", 'i', "ingotMithril", 's', "stickWood");
+        shaped(starsteelPaxelStack, "iii", " i ", " s ", 'i', "ingotStarsteel", 's', "stickWood");
+        shaped(aquariumPaxelStack, "iii", " i ", " s ", 'i', "ingotAquarium", 's', "stickWood");
     }
 
 }
